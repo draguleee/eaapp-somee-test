5d60161fb1c949dc823ecdf5638d52d9415cfba5
adragu <andredragu1699@gmail.com>
2025-09-24 19:15:00 +0300

    Add first Header tests

---
+++ eaapp-somee/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using eaapp_somee.pages.components;
+global using OpenQA.Selenium;
+global using OpenQA.Selenium.Chrome;
+global using eaapp_somee.utils;
+global using OpenQA.Selenium.Support.UI;
---
+++ eaapp-somee/utils/CustomMethods.cs
@@ -0,0 +1,10 @@
+﻿namespace eaapp_somee.utils
+{
+    public static class CustomMethods
+    {
+        public static void ClickElement(this IWebElement element)
+        {
+            element.Click();
+        }
+    }
+}
